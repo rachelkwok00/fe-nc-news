45f33a45e2cda3da86a8075e751e27929f6927cc
Pio Pang <piopang@pios-mbp.home>
2023-12-20 12:22:01 +0000

    In this commit I have fixed the sign in alert

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,26 +1,31 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import { CommentSvg , HeartSvg} from "../Assets/SvgFile";
+import Badge from "react-bootstrap/Badge";
+import Stack from "react-bootstrap/Stack";
 
 export default function ArticleCard(article) {
   return (
     <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Link href={`/articles/${article.article.article_id}`}>
+          <Card.Title>{article.article.title}</Card.Title>
+        </Card.Link>
+      </Card.Body>
+      <Badge pill bg="secondary" style={{ marginBottom: "10px" , width:"100px"}}>
+        {article.article.topic}
+      </Badge>
       <Card.Img variant="top" src={article.article.article_img_url} />
-      <Card.Body>
-        <Card.Link href={`/articles/${article.article.article_id}`}><Card.Title>{article.article.title}</Card.Title></Card.Link>
-       
-      </Card.Body>
+
       <ListGroup className="list-group-flush">
-        <ListGroup.Item>{article.article.topic}</ListGroup.Item>
-        <ListGroup.Item>{article.article.author}</ListGroup.Item>
-        <ListGroup.Item>Votes: {article.article.votes}</ListGroup.Item>
-        <ListGroup.Item>
-          Comments: {article.article.comment_count}
-        </ListGroup.Item>
+        <Card.Body>
+          {article.article.author}
+          <ListGroup.Item>
+            <HeartSvg /> {article.article.votes}
+            <CommentSvg /> {article.article.comment_count}
+          </ListGroup.Item>
+        </Card.Body>
       </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
     </Card>
   );
 }
